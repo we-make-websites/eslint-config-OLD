91dcbecfdcd6d5745a615be8d8b6e41db66e9243
Craig Baldwin <craig@wemakewebsites.com>
2021-08-02 08:35:05 +0100

    RELEASE: Initial release

---
+++ README.md
@@ -0,0 +1,19 @@
+# eslint-config
+
+We Make Website's eslint rules and config
+
+## Installation
+
+Install [eslint](https://eslint.org/) and `@we-make-websites/eslint-config`:
+
+```
+yarn add --dev eslint @we-make-websites/eslint-config
+```
+
+## Usage
+We Make Website’s eslint rules come bundled in `@we-make-websites/eslint-config`. To enable these rules, add a `eslint` property in your `package.json`. See the [eslint configuration docs](https://eslint.org/docs/user-guide/configuring/) for more details.
+```
+{
+  "extends": ["@we-make-websites/eslint-config"]
+}
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@shopify/esnext',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  root: true,
+  rules: {
+    'array-bracket-newline': ['error', {
+      minItems: 2,
+      multiline: true
+    }],
+    'array-element-newline': ['error', {
+      minItems: 2,
+      multiline: true
+    }],
+    'babel/camelcase': 'off',
+    'babel/object-curly-spacing': ['error', 'always'],
+    'babel/semi': ['error', 'never'],
+    'id-length': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'import/order': 'off',
+    'lines-around-comment': ['error', { ignorePattern: 'webpack' }],
+    'no-process-env': 'off',
+    'semi': ['error', 'never'],
+    'vue/no-v-html': 'off',
+  },
+}
