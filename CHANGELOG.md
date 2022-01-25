669ed62e12fba1618576dfcd4c84d028da2562eb
Craig Baldwin <craig@wemakewebsites.com>
2022-01-25 11:29:20 +0000

    REFACTOR: Removed multi-word component name rule

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 
 ## 2.4.0 - 2022-01-25
-* Added `eslint-plugin-vue-a11y` plugin
+* Removed multi-word component name rule
 * Updated dependencies
 
 ## 2.3.0 - 2021-11-09
--- index.js
+++ index.js
@@ -51,6 +51,7 @@ module.exports = {
       multiline: 'below',
     }],
     'vue/first-attribute-linebreak': 'error',
+    'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 'off',
     'vue/no-reserved-props': ['error', {
       vueVersion: 3,
