b631efe2f620d6e06c5948523858660786180dad
Craig Baldwin <craig@wemakewebsites.com>
2022-07-25 11:15:45 +0100

    BUILD: Added problem rules

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = {
   rules: merge(
     // require('./rules/formatting'),
     // require('./rules/import'),
-    require('./rules/problems'),
+    // require('./rules/problems'),
     // require('./rules/suggestions'),
     require('./rules/vue'),
     {
@@ -170,6 +170,27 @@ module.exports = {
         after: true,
         before: false,
       }]
+    },
+    {
+      'array-callback-return': 'error',
+      'no-await-in-loop': 'warn',
+      'no-constant-binary-expression': 'error',
+      'no-constructor-return': 'error',
+      'no-duplicate-imports': 'error',
+      'no-promise-executor-return': 'error',
+      'no-self-compare': 'error',
+      'no-template-curly-in-string': 'error',
+      'no-unmodified-loop-condition': 'error',
+      'no-unreachable-loop': 'error',
+      'no-unused-private-class-members': 'error',
+      'no-use-before-define': ['error', {
+        allowNamedExports: false,
+        classes: true,
+        functions: false,
+        variables: true,
+      }],
+      // Don't fully understand this rule
+      'require-atomic-updates': 'off',
     },
     {
       'accessor-pairs': ['warn', {
--- rules/problems.js
+++ rules/problems.js
@@ -1,5 +1,21 @@
 module.exports = {
-  'no-undef': 'error',
-  'no-unused-vars': 'off',
-  'no-var': 'error',
+  'array-callback-return': 'error',
+  'no-await-in-loop': 'warn',
+  'no-constant-binary-expression': 'error',
+  'no-constructor-return': 'error',
+  'no-duplicate-imports': 'error',
+  'no-promise-executor-return': 'error',
+  'no-self-compare': 'error',
+  'no-template-curly-in-string': 'error',
+  'no-unmodified-loop-condition': 'error',
+  'no-unreachable-loop': 'error',
+  'no-unused-private-class-members': 'error',
+  'no-use-before-define': ['error', {
+    allowNamedExports: false,
+    classes: true,
+    functions: false,
+    variables: true,
+  }],
+  // Don't fully understand this rule
+  'require-atomic-updates': 'off',
 }
